272b98c304b3035d3e21dd99da450098c5e6f2ae
EyosiSol <eyosila123@gmail.com>
2024-07-22 11:50:34 +0300

    initialize the basic block

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+};
